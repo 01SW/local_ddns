261c44a2e386d25d8d790d71c40c0ba7932e03a6
ws01 <01@ws01.ltd>
2023-02-12 18:24:24 +0800

    基本完成功能

---
+++ aliyun/aliyun_ddns.go
@@ -0,0 +1,232 @@
+package aliyun_ddns
+
+import (
+	"errors"
+	"fmt"
+	alidns20150109 "github.com/alibabacloud-go/alidns-20150109/v4/client"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	util "github.com/alibabacloud-go/tea-utils/v2/service"
+	"github.com/alibabacloud-go/tea/tea"
+	"net"
+	"strings"
+	"time"
+)
+
+// AccessKey 阿里云访问密钥与域名
+type AccessKey struct {
+	Id        string
+	KeySecret string
+	Endpoint  string
+}
+
+// Domain 域名相关结构体
+type Domain struct {
+	recordId string //阿里云域名修改时所需要的id，将根据Name自动查询到
+	value    string //将修改为的值
+
+	Name    string //域名后缀
+	Type    string //解析类型，一般的ipv4为A
+	RR      string //域名前缀
+	NetCard string //监测网卡名
+}
+
+type aliyun struct {
+	accessKey  AccessKey
+	client     *alidns20150109.Client
+	DomainList []Domain
+}
+
+// New 构造函数，保证按要求构建aliyun结构体
+func New(key AccessKey) (*aliyun, error) {
+	ptr := &aliyun{}
+	ptr.accessKey = key
+	er := ptr.createClient()
+	if er != nil {
+		return nil, er
+	}
+
+	return ptr, nil
+}
+
+// createClient 使用AccessKey初始化账号
+func (ptr *aliyun) createClient() error {
+	config := &openapi.Config{
+		AccessKeyId:     &ptr.accessKey.Id,
+		AccessKeySecret: &ptr.accessKey.KeySecret,
+	}
+	// 访问的域名
+	config.Endpoint = tea.String(ptr.accessKey.Endpoint)
+	ptr.client = &alidns20150109.Client{}
+	var err error
+	ptr.client, err = alidns20150109.NewClient(config)
+	return err
+}
+
+// AddDomains 添加将要监控的域名信息
+func (ptr *aliyun) AddDomains(domain Domain) error {
+	if domain.Name == "" || domain.RR == "" || domain.Type == "" || domain.NetCard == "" {
+		return errors.New("参数未填写完整")
+	}
+	if ptr.GetNetCardIP(domain.NetCard) == "" {
+		return errors.New("网卡名称错误，无法检测到此网卡")
+	}
+	er := ptr.searchRecordId(&domain)
+	if er != nil {
+		return er
+	}
+	ptr.DomainList = append(ptr.DomainList, domain)
+
+	return nil
+}
+
+// searchRecordId 查询RecordId
+func (ptr *aliyun) searchRecordId(domain *Domain) error {
+	describeDomainRecordsRequest := &alidns20150109.DescribeDomainRecordsRequest{
+		DomainName: tea.String(domain.Name),
+		RRKeyWord:  tea.String(domain.RR),
+	}
+	runtime := &util.RuntimeOptions{}
+	tryErr := func() (_e error) {
+		defer func() {
+			if r := tea.Recover(recover()); r != nil {
+				_e = r
+			}
+		}()
+		// 复制代码运行请自行打印 API 的返回值
+		request, _err := ptr.client.DescribeDomainRecordsWithOptions(describeDomainRecordsRequest, runtime)
+		if _err != nil {
+			return _err
+		}
+		requestStr := request.Body.String()
+		if strings.Index(requestStr, "DomainRecords") == -1 {
+			return errors.New(requestStr)
+		}
+		domain.recordId = *request.Body.DomainRecords.Record[0].RecordId
+
+		return nil
+	}()
+
+	if tryErr != nil {
+		var err = &tea.SDKError{}
+		if _t, ok := tryErr.(*tea.SDKError); ok {
+			err = _t
+		} else {
+			err.Message = tea.String(tryErr.Error())
+		}
+		_, _err := util.AssertAsString(err.Message)
+		if _err != nil {
+			return _err
+		}
+	}
+
+	return nil
+}
+
+// JudgeChange 判断当前域名是否需要修改解析
+func (ptr *aliyun) JudgeChange(domain *Domain, currIp string) (bool, error) {
+	if domain.value == currIp {
+		return false, nil
+	}
+	err := ptr.updateDomainRecord(domain, currIp)
+	if err != nil {
+		if domain.value == "" {
+			domain.value = currIp
+			//因为本程序不保存所修改的记录，所以每一次启动后此值都为空。都将会触发一次更新解析，如果云记录的数据与当前ip相等则
+			//updateDomainRecord函数将会返回错误，这个错误是不需要处理的
+			return false, nil
+		}
+		return true, err
+	}
+
+	domain.value = currIp
+	return true, nil
+}
+
+// updateDomainRecord 修改指定域名的解析
+func (ptr *aliyun) updateDomainRecord(domain *Domain, newIp string) error {
+	updateDomainRecordRequest := &alidns20150109.UpdateDomainRecordRequest{
+		RecordId: tea.String(domain.recordId),
+		Type:     tea.String(domain.Type),
+		Value:    tea.String(newIp),
+		RR:       tea.String(domain.RR),
+	}
+	runtime := &util.RuntimeOptions{}
+	tryErr := func() (_e error) {
+		defer func() {
+			if r := tea.Recover(recover()); r != nil {
+				_e = r
+			}
+		}()
+		re, _err := ptr.client.UpdateDomainRecordWithOptions(updateDomainRecordRequest, runtime)
+		if _err != nil {
+			return _err
+		}
+		jsonStr := re.Body.String()
+		if strings.Index(jsonStr, "Message") != -1 {
+			return errors.New(jsonStr)
+		}
+
+		return nil
+	}()
+
+	if tryErr != nil {
+		var err = &tea.SDKError{}
+		if _t, ok := tryErr.(*tea.SDKError); ok {
+			err = _t
+		} else {
+			err.Message = tea.String(tryErr.Error())
+		}
+		_, _err := util.AssertAsString(err.Message)
+		if _err != nil {
+			return _err
+		}
+	}
+	return nil
+}
+
+// GetNetCardIP 获取指定网卡ip
+func (ptr *aliyun) GetNetCardIP(name string) string {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		return ""
+	}
+
+	for i := 0; i < len(netInterfaces); i++ {
+		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
+			addrs, _ := netInterfaces[i].Addrs()
+
+			for _, address := range addrs {
+				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					if ipnet.IP.To4() != nil {
+						if netInterfaces[i].Name == name {
+							return ipnet.IP.String()
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return ""
+}
+
+// Start 检测域名与ip是否对应
+func (ptr *aliyun) Start() {
+	ticker1 := time.NewTicker(30 * time.Second)
+	defer ticker1.Stop()
+	for {
+		<-ticker1.C
+		for _, value := range ptr.DomainList {
+			currIp := ptr.GetNetCardIP(value.NetCard)
+			state, err := ptr.JudgeChange(&value, currIp)
+			if err != nil {
+				fmt.Println("修改域名解析失败，NetCard:", value.NetCard, " RR:", value.RR, " old ip:", value.value,
+					" new ip:", currIp)
+				continue
+			}
+			if state {
+				fmt.Println("成功修改解析，RR:", value.RR, " new ip:", currIp)
+			}
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"gopkg.in/ini.v1"
+	aliyun_ddns "local_ddns/aliyun"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func main() {
+	runPath := getRunPath()
+	if runPath == "" {
+		fmt.Println("启动路径获取失败")
+		return
+	}
+	c := "/"
+	if runtime.GOOS == "windows" {
+		c = "\\"
+	}
+	if !fileExists(runPath+c+"config"+c+"access.ini") ||
+		!fileExists(runPath+c+"config"+c+"domain.json") {
+		fmt.Println("配置文件缺失")
+		return
+	}
+
+	key, er := getAccessKey(runPath + c + "config" + c + "access.ini")
+	if er != nil {
+		fmt.Println("AccessKey数据获取失败")
+		return
+	}
+	ptr, err := aliyun_ddns.New(key)
+	if err != nil {
+		fmt.Println("初始化失败，", err.Error())
+		return
+	}
+
+	domainList := getJsonData(runPath + c + "config" + c + "domain.json")
+	if len(domainList) == 0 {
+		fmt.Println("解析json数据失败")
+		return
+	}
+	for _, value := range domainList {
+		err = ptr.AddDomains(value)
+		if err != nil {
+			fmt.Println("加入域名信息失败，", err.Error())
+			return
+		}
+	}
+
+	ptr.Start()
+}
+
+// 获取程序启动路径
+func getRunPath() string {
+	ex, err := os.Executable()
+	if err != nil {
+		return ""
+	}
+	exPath := filepath.Dir(ex)
+	return exPath
+}
+
+// 判断文件/文件夹是否存在
+func fileExists(path string) bool {
+	_, err := os.Stat(path) //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
+}
+
+// 获取accessKey
+func getAccessKey(path string) (aliyun_ddns.AccessKey, error) {
+	key := aliyun_ddns.AccessKey{}
+	cfg, err := ini.Load(path)
+	if err != nil {
+		return key, err
+	}
+	key.Id = cfg.Section("AccessKey").Key("Id").String()
+	key.KeySecret = cfg.Section("AccessKey").Key("KeySecret").String()
+	key.Endpoint = cfg.Section("AccessKey").Key("Endpoint").String()
+
+	return key, nil
+}
+
+// 获取json内部数据
+func getJsonData(path string) []aliyun_ddns.Domain {
+	var domainList []aliyun_ddns.Domain
+	data, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("json文件打开失败")
+		return domainList
+	}
+	err = json.Unmarshal(data, &domainList)
+	if err != nil {
+		fmt.Println("json转结构体失败")
+		return domainList
+	}
+	return domainList
+}
